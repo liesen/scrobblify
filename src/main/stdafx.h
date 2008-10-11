d7a19fbbf5b708a6029a309ebf96ea8175748227
johanliesen <johanliesen@8198a76c-954e-11dd-90d1-a37c03efd28a>
2008-10-11 16:32:46 +0000

    more source!
    git-svn-id: https://scrobblify.googlecode.com/svn/trunk@12 8198a76c-954e-11dd-90d1-a37c03efd28a

---
+++ src/main/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN // Excludes rarely-used stuff from Windows headers
+
+#include <windows.h>
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <string>
+#include <iostream>
